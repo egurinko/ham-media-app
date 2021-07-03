94204b3ecb1c76ce86da5028cfdfe6e54d586625
egurinko <egctoru@gmail.com>
2021-07-04 03:35:34 +0900

    remove unnecessaries

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,30 +1,9 @@
-const opts = {
-  schema: {
-    response: {
-      200: {
-        type: "object",
-        properties: {
-          hello: { type: "string" }
-        }
-      }
-    }
-  }
-}
+import { FastifyPluginCallback } from 'fastify'
 
-const routes = async (app, options) => {
-  const collection = app.mongo.db.collection("test_collection")
-  
+const router: FastifyPluginCallback = async (app, options) => {
   app.get("/", async (request, reply) => {
     return { hello: "world" }
   })
-
-  app.get("/animals/:animal", async (request, reply)=>{
-    const result = await collection.findOne({ animal: request.params.animal})
-    if(result === null){
-      throw new Error("Invalid value")
-    }
-    return result
-  })
 }
 
-export default routes
+export { router }
--- server/service/db-connector.ts
+++
@@ -1,9 +0,0 @@
-import fastifyPlugin from "fastify-plugin"
-
-const dbConnector = async (app, options) => {
-  app.register(import("fastify-mongodb"), {
-    url: "mongodb://localhost:27017/test_data_base"
-  })
-}
-
-export default fastifyPlugin(dbConnector)
