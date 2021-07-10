adebaf0df965b313be9e3da6347ae0ca81b6f06a
egurinko <egctoru@gmail.com>
2021-07-11 02:48:47 +0900

    remove unnecessary routing plugin

--- server/routes/internalApi/index.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyPluginCallback } from 'fastify'
-import { controller } from "../../controllers"
-
-const internalApiRouter: FastifyPluginCallback = async (app, _options) => {
-  const { internalApi } = controller
-  app.get("/internal_api/internal_users", internalApi.internalUsers.list)
-}
-
-export { internalApiRouter }
