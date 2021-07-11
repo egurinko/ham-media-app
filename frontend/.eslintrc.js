7f590934e9534fabd80cea044d4a1abefc92be76
egurinko <egctoru@gmail.com>
2021-07-11 21:36:19 +0900

    add react thing eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -24,6 +24,13 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
     'prettier',
   ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 0,
+  },
 };
