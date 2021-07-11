34d82a5d54e55c6371922bf6f0262d44ae153091
egurinko <egctoru@gmail.com>
2021-07-11 17:04:16 +0900

    expose internal user to graphql

--- server/graphql/internalApi/index.ts
+++ server/graphql/internalApi/index.ts
@@ -2,14 +2,17 @@ import { join } from "path"
 import { FastifyPluginCallback } from 'fastify'
 import mercurius from "mercurius"
 import { makeSchema } from "nexus"
-// import { Query } from "./query"
+import { $settings } from 'nexus-prisma'
+import { client } from "../../services/prisma"
+import * as types from "./types"
+import { Query } from "./query"
 
 const schema = makeSchema({
-  types: [],
+  types: [types, Query],
   outputs: {
     typegen: join(__dirname, "../../../graphql/internalApi", "types.d.ts"),
     schema: join(__dirname, "../../../graphql/internalApi", "schema.graphql"),
-  }
+  },
 })
 
 const internalApi: FastifyPluginCallback = async (app, _options) => {
@@ -17,7 +20,18 @@ const internalApi: FastifyPluginCallback = async (app, _options) => {
     routes: true,
     schema,
     graphiql: true,
+    context: (_request: any, _reply: any) => {
+      return {
+        prisma: client
+      }
+    }
+  })
+
+
+  $settings({
+    prismaClientContextField: 'prisma', // <-- Tell Nexus Prisma
   })
 }
+
 
 export { internalApi }
