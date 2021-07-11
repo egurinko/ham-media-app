c0a4abafdff4debce12aa86dbe3acc6ca3529d23
egurinko <egctoru@gmail.com>
2021-07-11 17:03:57 +0900

    add internal user to query

--- server/graphql/internalApi/query.ts
+++ server/graphql/internalApi/query.ts
@@ -1,9 +1,13 @@
 import { queryType } from "nexus"
+import { InternalUser } from "nexus-prisma"
 
 export const Query = queryType({
-  definition(_t){
-    // t.boolean("internalUsers", {
-    //   type: InternalUsers
-    // })
+  definition(t){
+    t.nonNull.list.nonNull.field("internalUsers", {
+      type: InternalUser.$name,
+      resolve(_, __, ctx) {
+        return ctx.prisma.internalUser.findMany()
+      },
+    })
   }
 })
