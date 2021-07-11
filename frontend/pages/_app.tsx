dae7bfc8f338319c97733913078823cad9d229dc
egurinko <egctoru@gmail.com>
2021-07-11 20:20:04 +0900

    add chakra

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { theme } from "../utils/theme"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeProvider
+        options={{
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
+  )
 }
 export default MyApp
