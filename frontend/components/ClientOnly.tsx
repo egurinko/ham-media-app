d196d0dc2537d33a07dc097271f4594445477f26
egurinko <egctoru@gmail.com>
2021-07-18 16:27:03 +0900

    add client only component

---
+++ frontend/components/ClientOnly.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+// ref: https://www.joshwcomeau.com/react/the-perils-of-rehydration/
+
+const ClientOnly: React.FC<{}> = ({ children, ...delegated }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <div {...delegated}>{children}</div>;
+};
+
+export default ClientOnly;
