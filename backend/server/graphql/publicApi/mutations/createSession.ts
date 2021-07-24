e3e6a96cf430ecc115eb49aa5620283ca67b1a0b
egurinko <egctoru@gmail.com>
2021-07-24 22:16:21 +0900

    create createSession resolver

---
+++ backend/server/graphql/publicApi/mutations/createSession.ts
@@ -0,0 +1,34 @@
+import { stringArg, nonNull, mutationField } from 'nexus';
+import { createSessionType } from '../types/createSessionType';
+import bcrypt from 'bcrypt';
+
+export const createSessionField = mutationField((t) => {
+  t.nonNull.field('createSession', {
+    type: createSessionType,
+    args: {
+      email: nonNull(stringArg()),
+      password: nonNull(stringArg()),
+    },
+    resolve: async (_, args, ctx) => {
+      const internalUser = await ctx.prisma.internalUser.findUnique({
+        where: {
+          email: args.email,
+        },
+      });
+
+      if (internalUser === null) throw new Error('Not Found');
+
+      const match = await bcrypt.compare(
+        args.password,
+        internalUser.password_digest
+      );
+
+      if (!match) {
+        throw new Error('Authentication Error');
+      }
+      return {
+        token: 'test token',
+      };
+    },
+  });
+});
