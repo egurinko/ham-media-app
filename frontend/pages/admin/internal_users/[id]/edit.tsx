99d088e7416409bd0ae61b6181df22258915a016
egurinko <egctoru@gmail.com>
2021-08-02 03:14:49 +0900

    add validation

--- frontend/pages/admin/internal_users/[id]/edit.tsx
+++ frontend/pages/admin/internal_users/[id]/edit.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { ParsedUrlQuery } from 'querystring';
 import {
@@ -9,10 +8,12 @@ import {
   Button,
   FormControl,
   FormLabel,
+  FormErrorMessage,
   Alert,
   AlertIcon,
 } from '@chakra-ui/react';
 import { useMutation } from '@apollo/client';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import InternalLayout from '@/components/admin/InternalLayout';
 import Card from '@/components/base/Card';
 import { getInternalUsers } from '@/api/internal_api/getInternalUsers';
@@ -29,15 +30,30 @@ import type {
 } from '@/api/internal_api/__generated__/GetInternalUser';
 import { internalApiClient } from '@/utils/apollo';
 
-const Edit: React.VFC<Props> = ({ internalUser }) => {
-  const [name, setName] = useState(internalUser.name);
-  const [email, setEmail] = useState(internalUser.email);
-  const [password, setPassword] = useState('');
+interface FormInput {
+  name: string;
+  email: string;
+  password: string;
+}
 
+const Edit: React.VFC<Props> = ({ internalUser }) => {
+  const {
+    control,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+  } = useForm<FormInput>({ mode: 'onTouched' });
   const [update, { data, loading }] = useMutation<
     UpdateInternalUser,
     UpdateInternalUserVariables
   >(updateInternalUser);
+
+  const onSubmit: SubmitHandler<FormInput> = ({ name, email, password }) => {
+    trigger();
+    update({
+      variables: { id: internalUser.id, name, email, password },
+    });
+  };
 
   return (
     <InternalLayout>
@@ -51,42 +67,77 @@ const Edit: React.VFC<Props> = ({ internalUser }) => {
         </>
       ) : null}
       <Card>
-        <form
-          onSubmit={async (e) => {
-            e.preventDefault();
-            update({
-              variables: { id: internalUser.id, name, email, password },
-            });
-          }}
-        >
+        <form onSubmit={handleSubmit(onSubmit)}>
           <Stack spacing={4}>
-            <FormControl id="name">
+            <FormControl id="name" isRequired isInvalid={!!errors.name}>
               <FormLabel>ユーザ名</FormLabel>
-              <Input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+              <Controller
+                name="name"
+                control={control}
+                defaultValue={internalUser.name}
+                rules={{ required: 'ユーザ名を入力してください' }}
+                render={({ field }) => (
+                  <Input type="text" isInvalid={!!errors.name} {...field} />
+                )}
               />
+              {errors.name && (
+                <FormErrorMessage>{errors.name.message}</FormErrorMessage>
+              )}
             </FormControl>
-            <FormControl id="email">
+            <FormControl id="email" isRequired isInvalid={!!errors.email}>
               <FormLabel>メールアドレス</FormLabel>
-              <Input
-                type="email"
-                autoComplete="email"
-                autoCapitalize="off"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+              <Controller
+                name="email"
+                control={control}
+                defaultValue={internalUser.email}
+                rules={{
+                  required: 'メールアドレスを入力してください',
+                  pattern: {
+                    value:
+                      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                    message: '有効なメールアドレスを入力してください',
+                  },
+                }}
+                render={({ field }) => (
+                  <Input
+                    type="email"
+                    autoComplete="email"
+                    autoCapitalize="off"
+                    isInvalid={!!errors.email}
+                    {...field}
+                  />
+                )}
               />
+              {errors.email && (
+                <FormErrorMessage>{errors.email.message}</FormErrorMessage>
+              )}
             </FormControl>
-            <FormControl id="password">
+            <FormControl id="password" isRequired isInvalid={!!errors.password}>
               <FormLabel>パスワード</FormLabel>
-              <Input
-                type="password"
-                autoComplete="current-password"
-                autoCapitalize="off"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+              <Controller
+                name="password"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: 'パスワードを入力してください',
+                  minLength: {
+                    value: 8,
+                    message: '8文字以上で入力してください',
+                  },
+                }}
+                render={({ field }) => (
+                  <Input
+                    type="password"
+                    autoComplete="current-password"
+                    autoCapitalize="off"
+                    isInvalid={!!errors.password}
+                    {...field}
+                  />
+                )}
               />
+              {errors.password && (
+                <FormErrorMessage>{errors.password.message}</FormErrorMessage>
+              )}
             </FormControl>
           </Stack>
           <Box d="grid" justifyContent="center">
@@ -98,6 +149,7 @@ const Edit: React.VFC<Props> = ({ internalUser }) => {
               color="white"
               type="submit"
               isLoading={loading}
+              disabled={!!errors.name || !!errors.email || !!errors.password}
             >
               更新
             </Button>
