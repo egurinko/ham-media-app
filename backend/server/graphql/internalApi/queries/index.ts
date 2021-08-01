0767c43aa2426d8b47b1dd902c328d780f4e62a1
egurinko <egctoru@gmail.com>
2021-08-01 16:28:11 +0900

    add session query

--- backend/server/graphql/internalApi/queries/index.ts
+++ backend/server/graphql/internalApi/queries/index.ts
@@ -1,1 +1,2 @@
 export * from './internalUsers';
+export * from './session';
---
+++ backend/server/graphql/internalApi/queries/session.ts
@@ -0,0 +1,17 @@
+import { queryField } from 'nexus';
+import { sessionType } from '../types/sessionType';
+import { sign } from '../../../services/authentication';
+
+export const sessionField = queryField((t) => {
+  t.nonNull.field('session', {
+    type: sessionType,
+    resolve: (_root, _args, ctx) => {
+      if (!ctx.currentInternalUser) {
+        throw new Error('Authentication Error');
+      }
+      return {
+        token: sign({ email: ctx.currentInternalUser.email }),
+      };
+    },
+  });
+});
