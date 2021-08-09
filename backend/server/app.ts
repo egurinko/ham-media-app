abebc89e40b79898606e6974b2f51fab435acc2f
egurinko <egctoru@gmail.com>
2021-08-09 12:07:28 +0900

    add bigint patch

--- backend/server/app.ts
+++ backend/server/app.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify';
 import cors from 'fastify-cors';
 import helmet from 'fastify-helmet';
+import 'json-bigint-patch';
 import { router } from './routes';
 
 const app = fastify({ logger: true });
