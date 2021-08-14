43f6385d791a1338cfdf558dec5b1d1fe822789a
egurinko <egctoru@gmail.com>
2021-08-15 01:19:40 +0900

    enable to show first hospital

---
+++ frontend/components/admin/hospitals/hospitalsStack.tsx
@@ -0,0 +1,98 @@
+import { Text, Skeleton, Box, VStack, Divider, Badge } from '@chakra-ui/react';
+import { useCallback, useState, Fragment } from 'react';
+import { useRouter } from 'next/router';
+import { useInternalGetHospitalConnectionQuery } from '@/api/internal_api/types';
+import MapPin from '../../../assets/map_pin.svg';
+import Phone from '../../../assets/phone.svg';
+
+const HospitalsStack: React.VFC<Record<string, never>> = () => {
+  const { data, loading, error } = useInternalGetHospitalConnectionQuery({
+    variables: { first: 10 },
+  });
+  const nodes = data?.hospitalConnection?.edges?.map((edge) => edge?.node);
+  const router = useRouter();
+
+  const handleClick = useCallback(
+    (id: BigInt) => {
+      router.push(`/admin/hospitals/${id}/edit`);
+    },
+    [router]
+  );
+
+  if (error) return <Text>エラーです</Text>;
+
+  return (
+    <>
+      <Skeleton isLoaded={!loading}>
+        <VStack spacing="0" mt="4" alignItems="flex-start">
+          <Divider />
+          {nodes?.map((node) => {
+            return node ? (
+              <Fragment key={node.name}>
+                <Box
+                  w="100%"
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="flex-start"
+                  _hover={{
+                    background: 'background.hover',
+                    color: 'primary.main',
+                    cursor: 'pointer',
+                  }}
+                  p="2"
+                  onClick={() => handleClick(node.id)}
+                >
+                  <Box w="full">
+                    <Box
+                      display="flex"
+                      flexDirection="row"
+                      justifyContent="space-between"
+                      alignItems="center"
+                      pb="2"
+                    >
+                      <Text
+                        fontSize={{ sm: 'xl', md: '2xl' }}
+                        fontWeight="bold"
+                      >
+                        {node.name}
+                      </Text>
+                      <div>
+                        {node.deleted ? (
+                          <Badge colorScheme="red">非公開</Badge>
+                        ) : (
+                          <Badge colorScheme="green">公開</Badge>
+                        )}
+                      </div>
+                    </Box>
+                  </Box>
+                  <Box flexShrink={0}>
+                    <Box flexShrink={0} display="flex" alignItems="center">
+                      <Box fill="primary.main" mr="1">
+                        <MapPin width={15} height={15} />
+                      </Box>
+                      <Text fontSize="sm">
+                        {node.hospitalAddress?.prefecture.name}
+                        {node.hospitalAddress?.address}
+                      </Text>
+                    </Box>
+                    <Box flexShrink={0} display="flex" alignItems="center">
+                      <Box fill="primary.main" mr="1">
+                        <Phone width={15} height={15} />
+                      </Box>
+                      <Text fontSize="sm">
+                        {node.hospitalAddress?.phone_number}
+                      </Text>
+                    </Box>
+                  </Box>
+                </Box>
+                <Divider />
+              </Fragment>
+            ) : null;
+          })}
+        </VStack>
+      </Skeleton>
+    </>
+  );
+};
+
+export default HospitalsStack;
---
+++ frontend/pages/admin/hospitals/index.tsx
@@ -0,0 +1,32 @@
+import { Heading, Box, Button } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
+import { useRouter } from 'next/router';
+import ClientOnly from '@/components/ClientOnly';
+import InternalLayout from '@/components/admin/InternalLayout';
+import HospitalsStack from '@/components/admin/hospitals/hospitalsStack';
+import { goAdminHospitalsNew } from '@/utils/routes';
+
+const Index: React.VFC<{}> = () => {
+  const router = useRouter();
+  return (
+    <InternalLayout>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Heading size="sm">病院一覧</Heading>
+        <Button
+          bgColor="primary.main"
+          color="white"
+          size="md"
+          leftIcon={<AddIcon />}
+          onClick={() => goAdminHospitalsNew(router)}
+        >
+          新規登録
+        </Button>
+      </Box>
+      <ClientOnly>
+        <HospitalsStack />
+      </ClientOnly>
+    </InternalLayout>
+  );
+};
+
+export default Index;
