365b2106e5c3746fc661b3b5fce57ab8f4c8b48c
egurinko <egctoru@gmail.com>
2021-09-11 17:19:43 +0900

    change port dynamically

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -1,8 +1,10 @@
 import { app } from './app';
 
+const PORT = process.env['PORT'] || 3000;
+
 const start = async () => {
   try {
-    await app.listen(3000);
+    await app.listen(PORT, '0.0.0.0');
   } catch (err) {
     app.log.error(err);
     process.exit(1);
