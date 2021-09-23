f5901de0abc31fd945535349a5ffd2846eef23a1
egurinko <egctoru@gmail.com>
2021-09-23 15:26:46 +0900

    separate presentation and logic

--- frontend/components/ecosystems/admin/login/Form.tsx
+++ frontend/components/ecosystems/admin/login/Form.tsx
@@ -1,37 +1,23 @@
 import { useEffect } from 'react';
-import {
-  Box,
-  Input,
-  Stack,
-  Button,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-} from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import { useForm, Controller, SubmitHandler } from 'react-hook-form';
-import Card from '@/components/atoms/Card';
-import FlashMessage from '@/components/molecules/FlashMessage';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import OrganismForm, {
+  FormInput,
+} from '@/components/organisms/admin/login/Form';
 import { usePublicCreateSessionMutation } from '@/api/public_api/types';
 import { setCookie } from '@/utils/cookies';
 import { goAdminInternalUsers } from '@/utils/routes';
-import validators from '@/validators/index';
-
-interface FormInput {
-  email: string;
-  password: string;
-}
 
 const Form: React.VFC<Record<string, never>> = () => {
+  const router = useRouter();
   const {
-    control,
-    handleSubmit,
+    control: RHFControl,
+    handleSubmit: RHFHandleSubmit,
     trigger,
-    formState: { errors },
+    formState: { errors: RHFErrors },
   } = useForm<FormInput>({ mode: 'onTouched' });
   const [login, { data: loginData, error: loginError }] =
     usePublicCreateSessionMutation();
-  const router = useRouter();
 
   const onSubmit: SubmitHandler<FormInput> = async ({ email, password }) => {
     trigger();
@@ -49,73 +35,14 @@ const Form: React.VFC<Record<string, never>> = () => {
   }, [loginData, loginError]);
 
   return (
-    <>
-      {loginData ? (
-        <FlashMessage message="ログインに成功しました。" status="success" />
-      ) : loginError ? (
-        <FlashMessage message={loginError.message} status="error" />
-      ) : null}
-      <Card>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Stack spacing={4}>
-            <FormControl id="email" isRequired isInvalid={!!errors.email}>
-              <FormLabel>メールアドレス</FormLabel>
-              <Controller
-                name="email"
-                control={control}
-                defaultValue=""
-                rules={validators.email.rules}
-                render={({ field }) => (
-                  <Input
-                    type="email"
-                    autoComplete="email"
-                    autoCapitalize="off"
-                    isInvalid={!!errors.email}
-                    {...field}
-                  />
-                )}
-              />
-              {errors.email && (
-                <FormErrorMessage>{errors.email.message}</FormErrorMessage>
-              )}
-            </FormControl>
-            <FormControl id="password" isRequired isInvalid={!!errors.password}>
-              <FormLabel>パスワード</FormLabel>
-              <Controller
-                name="password"
-                control={control}
-                defaultValue=""
-                rules={validators.password.rules}
-                render={({ field }) => (
-                  <Input
-                    type="password"
-                    autoComplete="current-password"
-                    autoCapitalize="off"
-                    isInvalid={!!errors.password}
-                    {...field}
-                  />
-                )}
-              />
-              {errors.password && (
-                <FormErrorMessage>{errors.password.message}</FormErrorMessage>
-              )}
-            </FormControl>
-          </Stack>
-          <Box d="grid" justifyContent="center">
-            <Button
-              size="lg"
-              mt="16"
-              variant="solid"
-              bgColor="primary.main"
-              color="white"
-              type="submit"
-            >
-              ログイン
-            </Button>
-          </Box>
-        </form>
-      </Card>
-    </>
+    <OrganismForm
+      isLoginSucceeded={!!loginData}
+      isLoginFailure={!!loginError}
+      loginErrorMessage={loginError?.message}
+      onSubmit={RHFHandleSubmit(onSubmit)}
+      control={RHFControl}
+      formErrors={RHFErrors}
+    />
   );
 };
 
---
+++ frontend/components/organisms/admin/login/Form.tsx
@@ -0,0 +1,111 @@
+import { FormEventHandler } from 'react';
+import {
+  Box,
+  Input,
+  Stack,
+  Button,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+} from '@chakra-ui/react';
+import { Controller, Control, DeepMap, FieldError } from 'react-hook-form';
+import Card from '@/components/atoms/Card';
+import FlashMessage from '@/components/molecules/FlashMessage';
+import validators from '@/validators/index';
+
+type Props = {
+  isLoginSucceeded: boolean;
+  isLoginFailure: boolean;
+  loginErrorMessage?: string;
+  onSubmit: FormEventHandler<HTMLFormElement>;
+  control: Control<FormInput>;
+  formErrors: DeepMap<FormInput, FieldError>;
+};
+
+export interface FormInput {
+  email: string;
+  password: string;
+}
+
+const Form: React.VFC<Props> = ({
+  isLoginSucceeded,
+  isLoginFailure,
+  loginErrorMessage,
+  onSubmit,
+  control,
+  formErrors,
+}) => (
+  <>
+    {isLoginSucceeded ? (
+      <FlashMessage message="ログインに成功しました。" status="success" />
+    ) : isLoginFailure ? (
+      <FlashMessage message={loginErrorMessage || ''} status="error" />
+    ) : null}
+    <Card>
+      <form onSubmit={onSubmit}>
+        <Stack spacing={4}>
+          <FormControl id="email" isRequired isInvalid={!!formErrors.email}>
+            <FormLabel>メールアドレス</FormLabel>
+            <Controller
+              name="email"
+              control={control}
+              defaultValue=""
+              rules={validators.email.rules}
+              render={({ field }) => (
+                <Input
+                  type="email"
+                  autoComplete="email"
+                  autoCapitalize="off"
+                  isInvalid={!!formErrors.email}
+                  {...field}
+                />
+              )}
+            />
+            {formErrors.email && (
+              <FormErrorMessage>{formErrors.email.message}</FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl
+            id="password"
+            isRequired
+            isInvalid={!!formErrors.password}
+          >
+            <FormLabel>パスワード</FormLabel>
+            <Controller
+              name="password"
+              control={control}
+              defaultValue=""
+              rules={validators.password.rules}
+              render={({ field }) => (
+                <Input
+                  type="password"
+                  autoComplete="current-password"
+                  autoCapitalize="off"
+                  isInvalid={!!formErrors.password}
+                  {...field}
+                />
+              )}
+            />
+            {formErrors.password && (
+              <FormErrorMessage>{formErrors.password.message}</FormErrorMessage>
+            )}
+          </FormControl>
+        </Stack>
+        <Box d="grid" justifyContent="center">
+          <Button
+            size="lg"
+            mt="16"
+            variant="solid"
+            bgColor="primary.main"
+            color="white"
+            type="submit"
+          >
+            ログイン
+          </Button>
+        </Box>
+      </form>
+    </Card>
+  </>
+);
+
+export default Form;
