fbbee30b7040b0208bdfd011cfc37020667db75b
egurinko <egctoru@gmail.com>
2021-09-25 18:32:16 +0900

    enable to accept children

--- frontend/components/atoms/Card.tsx
+++ frontend/components/atoms/Card.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@chakra-ui/react';
 
-const Card: React.FC<NoProps> = ({ children }) => {
+const Card: React.FC<{}> = ({ children }) => {
   return (
     <Box bg="white" shadow="md" width="full" p="6">
       {children}
