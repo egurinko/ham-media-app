b938a139e8aa75d9e024d3a4601467765ee7594a
egurinko <egctoru@gmail.com>
2021-09-26 19:41:09 +0900

    show hospital information

---
+++ frontend/components/organisms/consumer/hospitals/HospitalTags.tsx
@@ -0,0 +1,34 @@
+import { Tag, HStack } from '@chakra-ui/react';
+import type { HospitalFieldsFragment } from '@/api/public_api/types';
+
+type Props = {
+  hospital: HospitalFieldsFragment;
+  spacing: number;
+};
+
+const HospitalTags: React.FC<Props> = ({ hospital, spacing }) => (
+  <HStack spacing={spacing}>
+    {hospital.hospitalNightServiceOption?.status === '○' ? (
+      <Tag bgColor="primary.main" color="white">
+        夜間営業可
+      </Tag>
+    ) : null}
+    {hospital.hospitalReservationStatus?.required === '○' ? (
+      <Tag bgColor="primary.main" color="white">
+        予約必須
+      </Tag>
+    ) : null}
+    {hospital.hospitalReservationStatus?.reservable === '○' ? (
+      <Tag bgColor="primary.main" color="white">
+        予約可
+      </Tag>
+    ) : null}
+    {hospital.hospitalBusinessForm?.insurance_enabled === '○' ? (
+      <Tag bgColor="primary.main" color="white">
+        保険利用可
+      </Tag>
+    ) : null}
+  </HStack>
+);
+
+export default HospitalTags;
