88b7020c37971c12cb80e0e94712e5e02dbdd185
egurinko <egctoru@gmail.com>
2021-09-27 01:10:35 +0900

    add filter icon

---
+++ frontend/components/atoms/assets/FilterIcon.tsx
@@ -0,0 +1,12 @@
+import FilterSvg from '../../../assets/filter.svg';
+
+type Props = {
+  width: number;
+  height: number;
+};
+
+const FilterIcon: React.VFC<Props> = ({ width, height }) => (
+  <FilterSvg width={width} height={height} />
+);
+
+export default FilterIcon;
