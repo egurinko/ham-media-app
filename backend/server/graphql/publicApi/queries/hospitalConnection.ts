1445ffd6ef2b50766b304d9269eb269fa1d8b713
egurinko <egctoru@gmail.com>
2021-10-03 17:55:44 +0900

    enable to sort by place

--- backend/server/graphql/publicApi/queries/hospitalConnection.ts
+++ backend/server/graphql/publicApi/queries/hospitalConnection.ts
@@ -5,6 +5,7 @@
 import { queryField, nonNull, stringArg, booleanArg } from 'nexus';
 import { connectionFromArray } from 'graphql-relay';
 import { hospitalType } from '../types';
+import { getGeoLocation } from '@/services/api/googleApi';
 
 export const hospitalConnection = queryField((t) => {
   t.connectionField('publicHospitalConnection', {
@@ -18,27 +19,69 @@ export const hospitalConnection = queryField((t) => {
       nichijuOption: nonNull(booleanArg()),
     },
     resolve: async (_root, args, ctx) => {
-      return connectionFromArray(
-        await ctx.prisma.hospital.findMany({
-          where: {
-            deleted: false,
-            hospitalReservationStatus: {
-              reservable: args.reservable ? '○' : undefined,
-            },
-            hospitalNightServiceOption: {
-              status: args.nightServiceOption ? '○' : undefined,
-            },
-            hospitalBusinessForm: {
-              insurance_enabled: args.insuranceEnabled ? '○' : undefined,
-            },
-            hospitalCertificationOption: {
-              jsava_registered: args.jsavaOption ? '○' : undefined,
-              nichiju_registered: args.nichijuOption ? '○' : undefined,
+      const hospitals = await ctx.prisma.hospital.findMany({
+        where: {
+          deleted: false,
+          hospitalReservationStatus: {
+            reservable: args.reservable ? '○' : undefined,
+          },
+          hospitalNightServiceOption: {
+            status: args.nightServiceOption ? '○' : undefined,
+          },
+          hospitalBusinessForm: {
+            insurance_enabled: args.insuranceEnabled ? '○' : undefined,
+          },
+          hospitalCertificationOption: {
+            jsava_registered: args.jsavaOption ? '○' : undefined,
+            nichiju_registered: args.nichijuOption ? '○' : undefined,
+          },
+        },
+        select: {
+          id: true,
+          name: true,
+          internal_memo: true,
+          url: true,
+          hospitalAddress: {
+            select: {
+              hospitalAddressGeoLocation: {
+                select: {
+                  latitude: true,
+                  longitude: true,
+                },
+              },
             },
           },
-        }),
-        args
-      );
+        },
+      });
+
+      if (args.searchText !== '') {
+        const result = await getGeoLocation(args.searchText);
+        const location = result.data.results[0]?.geometry.location;
+        if (location) {
+          const sorted = hospitals
+            .filter(
+              (hospital) =>
+                !!hospital.hospitalAddress?.hospitalAddressGeoLocation
+            )
+            .sort((a, b) => {
+              const aLocation = a.hospitalAddress?.hospitalAddressGeoLocation;
+              const bLocation = b.hospitalAddress?.hospitalAddressGeoLocation;
+              if (aLocation && bLocation) {
+                const aDistance =
+                  (aLocation.latitude - location.lat) ** 2 +
+                  (aLocation.longitude - location.lng) ** 2;
+                const bDistance =
+                  (bLocation.latitude - location.lat) ** 2 +
+                  (bLocation.longitude - location.lng) ** 2;
+                return aDistance - bDistance;
+              }
+              return 0;
+            });
+          return connectionFromArray(sorted, args);
+        }
+      }
+
+      return connectionFromArray(hospitals, args);
     },
   });
 });
