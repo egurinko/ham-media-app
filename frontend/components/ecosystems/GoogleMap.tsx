55db13ebf9e3906125df9e9de0a582339b794dbb
egurinko <egctoru@gmail.com>
2021-10-10 16:05:16 +0900

    show current location in google map

--- frontend/components/ecosystems/GoogleMap.tsx
+++ frontend/components/ecosystems/GoogleMap.tsx
@@ -1,49 +1,69 @@
-import { useCallback, useState, memo } from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
+import { memo } from 'react';
+import { Spinner, Box } from '@chakra-ui/react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import {
+  PublicGetHospitalLocationsQuery,
+  usePublicGetHospitalLocationsQuery,
+} from '@/api/public_api/types';
 
 type Props = {
   width?: number;
   height?: number;
+  currentLat: number;
+  currentLng: number;
 };
 
-const GoogleMapComponent: React.FC<Props> = ({ children, height, width }) => {
-  const { isLoaded } = useJsApiLoader({
+const GoogleMapComponent: React.FC<Props> = ({
+  height,
+  width,
+  currentLat,
+  currentLng,
+}) => {
+  const { data: hospitalLoations } = usePublicGetHospitalLocationsQuery();
+  const { isLoaded, loadError } = useJsApiLoader({
     id: 'google-map-id',
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY || '',
   });
-  const [map, setMap] = useState(null);
 
-  const onLoad = useCallback((map) => {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map);
-  }, []);
+  const renderMap = () => {
+    const onLoad = (map: google.maps.Map) => {};
+    return (
+      <GoogleMap
+        mapContainerStyle={{
+          width: width ? `${width}px` : '100%',
+          height: height ? `${height}px` : '100%',
+        }}
+        center={{
+          lat: currentLat,
+          lng: currentLng,
+        }}
+        zoom={10}
+        onLoad={onLoad}
+      >
+        <Marker
+          position={{ lat: currentLat, lng: currentLng }}
+          animation={google.maps.Animation.DROP}
+          icon="https://user-images.githubusercontent.com/23233648/136685502-4bf03930-df2c-4194-8cc7-67f10699f5b8.png"
+        />
+      </GoogleMap>
+    );
+  };
 
-  const onUnmount = useCallback((map) => {
-    setMap(null);
-  }, []);
+  if (loadError) {
+    return <div>Map cannot be loaded right now, sorry.</div>;
+  }
 
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={{
-        width: width ? `${width}px` : '100%',
-        height: height ? `${height}px` : '100%',
-      }}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      {/* Child components, such as markers, info windows, etc. */}
-      <></>
-    </GoogleMap>
+    renderMap()
   ) : (
-    <></>
+    <Box textAlign="center">
+      <Spinner
+        thickness="4px"
+        emptyColor="gray.200"
+        color="primary.main"
+        size="lg"
+      />
+    </Box>
   );
 };
 
