fdd0f46c147c0198f004b25ebeeca22d03720965
egurinko <egctoru@gmail.com>
2021-10-10 16:49:51 +0900

    add infoWindow

--- frontend/components/ecosystems/GoogleMap.tsx
+++ frontend/components/ecosystems/GoogleMap.tsx
@@ -1,6 +1,11 @@
-import { memo } from 'react';
-import { Spinner, Box } from '@chakra-ui/react';
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { memo, useState, useCallback } from 'react';
+import { Spinner, Box, Text } from '@chakra-ui/react';
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api';
 import FlashMessage from '@/components/molecules/FlashMessage';
 import {
   PublicGetHospitalLocationsQuery,
@@ -26,30 +31,6 @@ const GoogleMapComponent: React.FC<Props> = ({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY || '',
   });
 
-  const renderMap = () => {
-    const onLoad = (map: google.maps.Map) => {};
-    return (
-      <GoogleMap
-        mapContainerStyle={{
-          width: width ? `${width}px` : '100%',
-          height: height ? `${height}px` : '100%',
-        }}
-        center={{
-          lat: currentLat,
-          lng: currentLng,
-        }}
-        zoom={10}
-        onLoad={onLoad}
-      >
-        <Marker
-          position={{ lat: currentLat, lng: currentLng }}
-          animation={google.maps.Animation.DROP}
-          icon="https://user-images.githubusercontent.com/23233648/136685502-4bf03930-df2c-4194-8cc7-67f10699f5b8.png"
-        />
-      </GoogleMap>
-    );
-  };
-
   if (loadError) {
     return (
       <FlashMessage
@@ -60,7 +41,13 @@ const GoogleMapComponent: React.FC<Props> = ({
   }
 
   return isLoaded ? (
-    renderMap()
+    <Map
+      height={height}
+      width={width}
+      currentLat={currentLat}
+      currentLng={currentLng}
+      hospitalLoations={hospitalLoations?.hospitals}
+    />
   ) : (
     <Box textAlign="center">
       <Spinner
@@ -70,6 +57,98 @@ const GoogleMapComponent: React.FC<Props> = ({
         size="lg"
       />
     </Box>
+  );
+};
+
+type HospitalLocation = PublicGetHospitalLocationsQuery['hospitals'][number];
+
+type MapProps = Props & {
+  hospitalLoations?: HospitalLocation[];
+};
+
+const Map: React.FC<MapProps> = ({
+  height,
+  width,
+  currentLat,
+  currentLng,
+  hospitalLoations,
+}) => {
+  const [currentHospital, setCurrentHospital] =
+    useState<null | HospitalLocation>(null);
+  const handleHospitalClick = useCallback((hospital: HospitalLocation) => {
+    setCurrentHospital(hospital);
+  }, []);
+  const handleInfoWindoClose = useCallback(() => {
+    setCurrentHospital(null);
+  }, []);
+
+  const onLoad = (map: google.maps.Map) => {};
+  return (
+    <GoogleMap
+      mapContainerStyle={{
+        width: width ? `${width}px` : '100%',
+        height: height ? `${height}px` : '100%',
+      }}
+      center={{
+        lat: currentLat,
+        lng: currentLng,
+      }}
+      zoom={12}
+      onLoad={onLoad}
+    >
+      <>
+        <Marker
+          position={{ lat: currentLat, lng: currentLng }}
+          animation={google.maps.Animation.DROP}
+          icon="https://user-images.githubusercontent.com/23233648/136685502-4bf03930-df2c-4194-8cc7-67f10699f5b8.png"
+        />
+        {hospitalLoations?.map((h) => {
+          if (
+            h.hospitalAddress &&
+            h.hospitalAddress.hospitalAddressGeoLocation
+          ) {
+            return (
+              <Marker
+                key={String(h.id)}
+                position={{
+                  lat: h.hospitalAddress.hospitalAddressGeoLocation.latitude,
+                  lng: h.hospitalAddress.hospitalAddressGeoLocation.longitude,
+                }}
+                onClick={() => handleHospitalClick(h)}
+              />
+            );
+          }
+        })}
+        {currentHospital ? (
+          <InfoWindow
+            position={{
+              lat:
+                currentHospital.hospitalAddress!.hospitalAddressGeoLocation!
+                  .latitude + 0.012,
+              lng: currentHospital.hospitalAddress!.hospitalAddressGeoLocation!
+                .longitude,
+            }}
+            onCloseClick={handleInfoWindoClose}
+          >
+            <Box display="flex" flexDirection="column">
+              <a href={currentHospital.url}>
+                <Text fontSize="md" textDecoration="underline">
+                  {currentHospital.name}
+                </Text>
+              </a>
+              <Text fontSize="xs">
+                {currentHospital.hospitalAddress?.phone_number}
+              </Text>
+              <Text fontSize="xs">
+                {currentHospital.hospitalAddress?.address}
+              </Text>
+            </Box>
+          </InfoWindow>
+        ) : (
+          <></>
+        )}
+      </>
+    </GoogleMap>
   );
 };
 
