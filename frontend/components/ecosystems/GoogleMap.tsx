b23454798ce066e1b2f825aabec7b9ca7e4f6882
egurinko <egctoru@gmail.com>
2021-10-10 16:52:10 +0900

    remove unnecessary

--- frontend/components/ecosystems/GoogleMap.tsx
+++ frontend/components/ecosystems/GoogleMap.tsx
@@ -81,8 +81,6 @@ const Map: React.FC<MapProps> = ({
   const handleInfoWindoClose = useCallback(() => {
     setCurrentHospital(null);
   }, []);
-
-  const onLoad = (map: google.maps.Map) => {};
   return (
     <GoogleMap
       mapContainerStyle={{
@@ -94,60 +92,54 @@ const Map: React.FC<MapProps> = ({
         lng: currentLng,
       }}
       zoom={12}
-      onLoad={onLoad}
     >
-      <>
-        <Marker
-          position={{ lat: currentLat, lng: currentLng }}
-          animation={google.maps.Animation.DROP}
-          icon="https://user-images.githubusercontent.com/23233648/136685502-4bf03930-df2c-4194-8cc7-67f10699f5b8.png"
-        />
-        {hospitalLoations?.map((h) => {
-          if (
-            h.hospitalAddress &&
-            h.hospitalAddress.hospitalAddressGeoLocation
-          ) {
-            return (
-              <Marker
-                key={String(h.id)}
-                position={{
-                  lat: h.hospitalAddress.hospitalAddressGeoLocation.latitude,
-                  lng: h.hospitalAddress.hospitalAddressGeoLocation.longitude,
-                }}
-                onClick={() => handleHospitalClick(h)}
-              />
-            );
-          }
-        })}
-        {currentHospital ? (
-          <InfoWindow
-            position={{
-              lat:
-                currentHospital.hospitalAddress!.hospitalAddressGeoLocation!
-                  .latitude + 0.012,
-              lng: currentHospital.hospitalAddress!.hospitalAddressGeoLocation!
-                .longitude,
-            }}
-            onCloseClick={handleInfoWindoClose}
-          >
-            <Box display="flex" flexDirection="column">
-              <a href={currentHospital.url}>
-                <Text fontSize="md" textDecoration="underline">
-                  {currentHospital.name}
-                </Text>
-              </a>
-              <Text fontSize="xs">
-                {currentHospital.hospitalAddress?.phone_number}
+      <Marker
+        position={{ lat: currentLat, lng: currentLng }}
+        animation={google.maps.Animation.DROP}
+        icon="https://user-images.githubusercontent.com/23233648/136685502-4bf03930-df2c-4194-8cc7-67f10699f5b8.png"
+      />
+      {hospitalLoations?.map((h) => {
+        if (h.hospitalAddress && h.hospitalAddress.hospitalAddressGeoLocation) {
+          return (
+            <Marker
+              key={String(h.id)}
+              position={{
+                lat: h.hospitalAddress.hospitalAddressGeoLocation.latitude,
+                lng: h.hospitalAddress.hospitalAddressGeoLocation.longitude,
+              }}
+              onClick={() => handleHospitalClick(h)}
+            />
+          );
+        }
+      })}
+      {currentHospital ? (
+        <InfoWindow
+          position={{
+            lat:
+              currentHospital.hospitalAddress!.hospitalAddressGeoLocation!
+                .latitude + 0.012,
+            lng: currentHospital.hospitalAddress!.hospitalAddressGeoLocation!
+              .longitude,
+          }}
+          onCloseClick={handleInfoWindoClose}
+        >
+          <Box display="flex" flexDirection="column">
+            <a href={currentHospital.url}>
+              <Text fontSize="md" textDecoration="underline">
+                {currentHospital.name}
               </Text>
-              <Text fontSize="xs">
-                {currentHospital.hospitalAddress?.address}
-              </Text>
-            </Box>
-          </InfoWindow>
-        ) : (
-          <></>
-        )}
-      </>
+            </a>
+            <Text fontSize="xs">
+              {currentHospital.hospitalAddress?.phone_number}
+            </Text>
+            <Text fontSize="xs">
+              {currentHospital.hospitalAddress?.address}
+            </Text>
+          </Box>
+        </InfoWindow>
+      ) : (
+        <></>
+      )}
     </GoogleMap>
   );
 };
