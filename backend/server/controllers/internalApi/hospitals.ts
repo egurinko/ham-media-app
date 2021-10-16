0396f3e4d72c18c6ba6d133929337e82898d1c88
egurinko <egctoru@gmail.com>
2021-10-17 01:39:17 +0900

    remove unnecessary controllers

--- backend/server/controllers/internalApi/hospitals.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyRequest, FastifyReply } from 'fastify';
-
-const list = async (_request: FastifyRequest, _reply: FastifyReply) => {
-  return { hello: 'world' };
-};
-
-export const hospitals = {
-  list,
-};
--- backend/server/controllers/publicApi/index.ts
+++
@@ -1,5 +0,0 @@
-import { hospitals } from './hospitals';
-
-export const publicApi = {
-  hospitals,
-};
