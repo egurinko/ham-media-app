5eab2a1897b9c9ce4c6ae02081f934fa6ddc024e
egurinko <egctoru@gmail.com>
2021-10-17 01:40:51 +0900

    setup line sdk

---
+++ backend/server/services/line.ts
@@ -0,0 +1,11 @@
+import * as line from '@line/bot-sdk';
+
+const config = {
+  channelAccessToken: process.env['LINE_CHANNEL_ACCESS_TOKEN'] || '',
+  channelSecret: process.env['LINE_CHANNEL_SECRET'] || '',
+};
+
+const client = new line.Client(config);
+const middleware = line.middleware(config);
+
+export { client as lineClient, middleware as lineMiddleware };
