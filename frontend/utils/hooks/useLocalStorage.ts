d255ca80e4ab10a0606df9b02b36ed5626c0121b
egurinko <egctoru@gmail.com>
2021-10-16 16:32:54 +0900

    create localStorage hooks

---
+++ frontend/utils/hooks/useLocalStorage.ts
@@ -0,0 +1,18 @@
+import { useCallback } from 'react';
+
+export const useLocalStorage = <T = any>(key: string) => {
+  const setLocalStorage = useCallback(
+    (value) => {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    },
+    [key]
+  );
+
+  const getLocalStorage = useCallback((): T | null => {
+    const stored = window.localStorage.getItem(key);
+
+    return stored ? JSON.parse(stored) : null;
+  }, [key]);
+
+  return { setLocalStorage, getLocalStorage };
+};
