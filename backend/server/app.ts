6491a2680442b863b739a865f81080d467c1d62b
egurinko <egctoru@gmail.com>
2021-10-31 15:44:42 +0900

    setup fastify cron

--- backend/server/app.ts
+++ backend/server/app.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify';
 import cors from 'fastify-cors';
 import helmet from 'fastify-helmet';
+import fastifyCron from 'fastify-cron';
 import 'json-bigint-patch';
 import { router } from './routes';
+import { jobs } from './services/jobs';
 
 const app = fastify({
   logger: {
@@ -14,6 +16,10 @@ app.register(helmet, {
   contentSecurityPolicy: false,
 });
 app.register(cors);
+app.register(fastifyCron, {
+  jobs,
+});
+
 app.register(router);
 
 export { app };
--- backend/server/index.ts
+++ backend/server/index.ts
@@ -5,6 +5,7 @@ const PORT = process.env['PORT'] || 3000;
 const start = async () => {
   try {
     await app.listen(PORT, '0.0.0.0');
+    app.cron.startAllJobs();
   } catch (err) {
     app.log.error(err);
     process.exit(1);
