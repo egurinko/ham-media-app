31a43e963741dbea88d0665e36dbbef206a482a0
egurinko <egctoru@gmail.com>
2021-11-28 22:00:27 +0900

    add tag new page

---
+++ frontend/pages/admin/product_tag_groups/new.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router';
+import { Heading, Box, IconButton } from '@chakra-ui/react';
+import { ChevronRightIcon } from '@chakra-ui/icons';
+import { InternalLayout } from '@/components/layouts/admin/InternalLayout';
+import { Form } from '@/components/organisms/admin/product_tag_groups/new/Form';
+import { goAdminProductTagGroups } from '@/utils/routes';
+
+const New: React.VFC<NoProps> = () => {
+  const router = useRouter();
+
+  return (
+    <InternalLayout>
+      <Box d="flex" mb="4">
+        <IconButton
+          aria-label="link"
+          variant="link"
+          onClick={() => goAdminProductTagGroups(router)}
+          icon={<ChevronRightIcon />}
+        />
+        <Heading size="sm">タグカテゴリー登録</Heading>
+      </Box>
+      <Form />
+    </InternalLayout>
+  );
+};
+
+export default New;
