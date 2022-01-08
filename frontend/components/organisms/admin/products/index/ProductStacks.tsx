6e411d693c8309d3ceb9ec4fcef7adcfbad6cd82
egurinko <egctoru@gmail.com>
2022-01-08 18:33:31 +0900

    separate ProductInfo from ProductStacks

--- frontend/components/organisms/admin/products/index/ProductStacks.tsx
+++ frontend/components/organisms/admin/products/index/ProductStacks.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useRef, useState, useMemo } from 'react';
 import {
-  Text,
   Skeleton,
   Box,
   VStack,
   Divider,
   Spinner,
-  Tag,
   Button,
   Input,
   FormControl,
@@ -16,8 +14,7 @@ import {
   RadioGroup,
   Stack,
 } from '@chakra-ui/react';
-import { useCallback, Fragment } from 'react';
-import { useRouter } from 'next/router';
+import { Fragment } from 'react';
 import { Card } from '@/components/atoms/Card';
 import {
   useInternalGetProductConnectionQuery,
@@ -27,7 +24,7 @@ import {
 } from '@/api/internal_api/types';
 import type { Product } from '@/api/internal_api/types';
 import { FlashMessage } from '@/components/molecules/FlashMessage';
-import { goAdminProductsEdit } from '@/utils/routes';
+import { ProductSummary } from './productStacks/ProductSummary';
 import { useIntersectionObserver } from '@/utils/hooks/useIntersectionObserver';
 
 const PRODUCT_STOCK = {
@@ -37,7 +34,6 @@ const PRODUCT_STOCK = {
 } as const;
 
 const ProductStacks: React.VFC<NoProps> = () => {
-  const router = useRouter();
   const [name, setName] = useState('');
   const [selectedMakerId, setSelectedMakerId] = useState<undefined | string>(
     undefined
@@ -98,13 +94,6 @@ const ProductStacks: React.VFC<NoProps> = () => {
     selectedInternalUserId,
     hasStock,
   ]);
-
-  const handleProductClick = useCallback(
-    (id: number) => {
-      goAdminProductsEdit(router, { id });
-    },
-    [router]
-  );
 
   const handleSearch = () => {
     fetchMore({
@@ -227,72 +216,7 @@ const ProductStacks: React.VFC<NoProps> = () => {
           <Divider />
           {nodes?.map((product) => (
             <Fragment key={product.id}>
-              <Box
-                w="100%"
-                display="flex"
-                flexDirection="row"
-                alignItems="center"
-                _hover={{
-                  background: 'background.hover',
-                  color: 'primary.main',
-                  cursor: 'pointer',
-                }}
-                p="2"
-                onClick={() => handleProductClick(product.id)}
-              >
-                <Box flex="1" display="flex" flexDir="row" alignItems="center">
-                  <Box flexShrink="0" mr="1">
-                    <img
-                      src={product.url}
-                      alt={product.name}
-                      width="80"
-                      height="80"
-                      style={{
-                        objectFit: 'contain',
-                        width: '80px',
-                        height: '80px',
-                      }}
-                    />
-                  </Box>
-                  <Box flexShrink="1">
-                    <Text fontSize="xs">{product.maker.name}</Text>
-                    <Text fontSize="sm" mb="2" fontWeight="bold">
-                      {product.name}
-                    </Text>
-                    {product.productTaggings.map((productTagging) => (
-                      <Tag
-                        key={productTagging.id}
-                        bgColor="primary.light"
-                        color="primary.main"
-                        fontSize="xs"
-                        mr="1"
-                        mb="2"
-                      >
-                        {productTagging.productTag.name}
-                      </Tag>
-                    ))}
-                    <Text fontSize="xs" mb="2">
-                      割当：
-                      {Array.from(
-                        new Set(
-                          product.stocks
-                            .filter((s) => s.stockAllocation)
-                            .map((s) => s.stockAllocation?.internalUser.name)
-                        )
-                      ).join(', ')}
-                    </Text>
-                  </Box>
-                </Box>
-                <Box ml="2">
-                  <Text fontSize="sm">総在庫：{product.totalStockAmount}</Text>
-                  <Text fontSize="sm">
-                    貸出数：{product.allocatedStockAmount}
-                  </Text>
-                  <Text fontSize="sm">
-                    残数：{product.remainingStockAmount}
-                  </Text>
-                </Box>
-              </Box>
+              <ProductSummary product={product} />
               <Divider />
             </Fragment>
           ))}
