dbf7278b1b6d4ab90de7ca0eee1dda3bee3be365
egurinko <egctoru@gmail.com>
2022-01-30 02:43:32 +0900

    add test import alias

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -11,6 +11,7 @@ export default async (): Promise<Config.InitialOptions> => {
     testEnvironment: 'node',
     moduleNameMapper: {
       '@/(.*)': '<rootDir>/server/$1',
+      '@tests/(.*)': '<rootDir>/tests/$1',
     },
     setupFilesAfterEnv: ['<rootDir>/tests/configs/setupDbConnection.ts'],
     testMatch: ['**/tests/**/*.spec.[jt]s'],
