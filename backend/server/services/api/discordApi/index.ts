6df4c9aca4e53c257e0d9a0c4aac822dd75847c6
egurinko <egctoru@gmail.com>
2022-02-26 16:27:56 +0900

    use \n for folding line

--- backend/server/services/api/discordApi/index.ts
+++ backend/server/services/api/discordApi/index.ts
@@ -19,11 +19,11 @@ const postStockExpirationAlert = (variables: PostStockAlertVariables): void => {
   const lines = variables
     .map(
       (v) =>
-        `\r${v.product.name}の在庫id:${v.id} =>  https://ham-media-app.net/admin/products/${v.product_id}`
+        `\n${v.product.name}の在庫id:${v.id} =>  https://ham-media-app.net/admin/products/${v.product_id}`
     )
     .join();
   postStockAlert(
-    `===== 以下の在庫の期限が切れているよ <:shock:844862291215122432> ===== ${lines}\r`
+    `===== 以下の在庫の期限が切れているよ <:shock:844862291215122432> ===== ${lines}\n`
   );
 };
 
@@ -31,12 +31,12 @@ const postStockExpiringAlert = (variables: PostStockAlertVariables): void => {
   const lines = variables
     .map(
       (v) =>
-        `\r${v.product.name}の在庫id:${v.id} =>  https://ham-media-app.net/admin/products/${v.product_id}`
+        `\n${v.product.name}の在庫id:${v.id} =>  https://ham-media-app.net/admin/products/${v.product_id}`
     )
     .join();
 
   postStockAlert(
-    `===== 今週はこの在庫の期限が切れちゃうよ <:doyouebest:844132000788643860> ===== ${lines}\r`
+    `===== 今週はこの在庫の期限が切れちゃうよ <:doyouebest:844132000788643860> ===== ${lines}\n`
   );
 };
 
@@ -50,9 +50,9 @@ const postStockRequestAlert = (
 ): void => {
   if (!DISCORD_STOCK_WEBHOOK_URL) return;
   const lines = stockRequest.productRegistrations
-    .map((productRegistration) => `\r・${productRegistration.product.name}`)
+    .map((productRegistration) => `\n・${productRegistration.product.name}`)
     .join();
-  const content = `${internalUser.name}さんが在庫リクエストをしたよ <:yeah:913316943921033247> https://ham-media-app.net/admin/stock_requests/${stockRequest.id}/edit ${lines}`;
+  const content = `${internalUser.name}さんが在庫リクエストをしたよ <:yeah:913316943921033247> \n https://ham-media-app.net/admin/stock_requests/${stockRequest.id}/edit ${lines}`;
   postStockAlert(content);
 };
 
@@ -67,9 +67,9 @@ const postStockRequestRejectionAlert = (
 ) => {
   if (!DISCORD_STOCK_WEBHOOK_URL) return;
   const lines = stockRequest.productRegistrations
-    .map((productRegistration) => `\r・${productRegistration.product.name}`)
+    .map((productRegistration) => `\n・${productRegistration.product.name}`)
     .join();
-  const content = `${internalUser.name}さんの以下の在庫リクエストが棄却されたよ <:jii:913298500861698058> ${lines} \r[棄却メッセージ]\r${message}`;
+  const content = `${internalUser.name}さんの以下の在庫リクエストが棄却されたよ <:jii:913298500861698058> ${lines} \n[棄却メッセージ]\n${message}`;
   postStockAlert(content);
 };
 
