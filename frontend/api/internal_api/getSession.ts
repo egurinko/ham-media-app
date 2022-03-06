ee11ac907d03713e1831a121c758672ba620fa4e
egurinko <egctoru@gmail.com>
2022-03-06 17:47:49 +0900

    get role of session internal user

--- frontend/api/internal_api/getSession.ts
+++ frontend/api/internal_api/getSession.ts
@@ -1,13 +1,13 @@
 import { gql } from '@apollo/client';
+import { INTERNAL_USER_FIELDS } from './fragments/internalUser';
 
 export const getSession = gql`
+  ${INTERNAL_USER_FIELDS}
   query InternalGetSession {
     session {
       token
       internalUser {
-        id
-        name
-        email
+        ...InternalUserFields
       }
     }
   }
