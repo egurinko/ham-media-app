4086100ac009375b348da4ba66ce8f04887aa7d3
egurinko <egctoru@gmail.com>
2022-03-06 15:41:29 +0900

    add lint rules

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,36 +1,62 @@
 module.exports = {
-  env: {
-    es2021: true,
-    browser: true,
-    node: true,
-  },
   settings: {
-    react: {
-      version: 'detect',
+    next: {
+      rootDir: '/frontend/*',
     },
   },
-  globals: {
-    window: true,
-    document: true,
-    navigator: true,
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
+  parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
+    'next/core-web-vitals',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
     'prettier',
+    'plugin:import/recommended',
   ],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 0,
+    '@next/next/no-img-element': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+        disallowTypeAnnotations: true,
+      },
+    ],
+    'arrow-body-style': ['error', 'as-needed'],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        pathGroups: [
+          {
+            pattern: '@/**/**',
+            group: 'parent',
+            position: 'before',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'ignore',
+      },
+    ],
+    'no-empty': ['error', { allowEmptyCatch: true }],
   },
 };
