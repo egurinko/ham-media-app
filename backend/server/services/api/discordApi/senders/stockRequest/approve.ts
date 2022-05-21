e66440a28ab6d4eba2bf0f7657bbd8830826b088
egurinko <egctoru@gmail.com>
2022-05-21 16:37:13 +0900

    show number of product instaed of showing lines

--- backend/server/services/api/discordApi/senders/stockRequest/approve.ts
+++ backend/server/services/api/discordApi/senders/stockRequest/approve.ts
@@ -1,4 +1,5 @@
 import { postStockAlert } from '@/services/api/discordApi/notifiers/stock';
+import { createStockRequestCounts } from './utils';
 import type { PostStockRequestAlertVariables } from './types';
 import type { InternalUser } from '@prisma/client';
 
@@ -7,10 +8,11 @@ export const postStockRequestApprovalAlert = (
   approvingInternalUser: InternalUser,
   message: string
 ) => {
-  const lines = stockRequest.productRegistrations
+  const stockRequestCounts = createStockRequestCounts(stockRequest);
+  const lines = stockRequestCounts
     .map(
-      (productRegistration) =>
-        `\n・${productRegistration.product.name} https://ham-media-app.net/admin/products/${productRegistration.product.id}`
+      (stockRequestCount) =>
+        `\n・${stockRequestCount.product.name}（https://ham-media-app.net/admin/products/${stockRequestCount.product.id}）：${stockRequestCount.count}つ`
     )
     .join();
   const content = `<@${stockRequest.internalUser.discord_user_id}> さんの在庫リクエストが <@${approvingInternalUser.discord_user_id}> さんに承認されたよ <:ok:776668642976071730> \n <@${approvingInternalUser.discord_user_id}> さんは在庫の割り当てをやってね🙊 ${lines} \n[承認メッセージ]\n${message}\n`;
