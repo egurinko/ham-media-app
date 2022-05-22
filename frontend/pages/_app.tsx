275f65d37888db33e5dc15da12afdeb365d1f77a
egurinko <egctoru@gmail.com>
2022-05-22 20:14:19 +0900

    create per page layout logic

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,20 +3,33 @@ import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 import 'json-bigint-patch';
 import { apiClient } from '@/utils/apollo';
 import { theme } from '@/utils/theme';
+import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import type { ReactElement } from 'react';
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <ApolloProvider client={apiClient}>
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
-  </ApolloProvider>
-);
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactElement;
+};
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const App: React.FC<AppPropsWithLayout> = ({ Component, pageProps }) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <ApolloProvider client={apiClient}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          {getLayout(<Component {...pageProps} />)}
+        </ColorModeProvider>
+      </ChakraProvider>
+    </ApolloProvider>
+  );
+};
 
 export default App;
