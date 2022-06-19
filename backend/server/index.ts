5183d1ee87f70cee2ff13193266e79218008e611
Toru Eguchi <egctoru@gmail.com>
2022-06-19 19:54:11 +0900

    Revert "Revert "Revert "fastify を v4 に upgrade"""

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -1,17 +1,11 @@
 import { app } from './app';
-import { stockExpirationAlertJob, geoLocationAlertJob } from '@/services/jobs';
 
-const PORT = Number(process.env['PORT']) || 3000;
+const PORT = process.env['PORT'] || 3000;
 
 const start = async () => {
   try {
-    await app.listen({ port: PORT });
-    console.log(`server start listening in port:${PORT}`);
-
-    app.ready().then(() => {
-      stockExpirationAlertJob.start();
-      geoLocationAlertJob.start();
-    });
+    await app.listen(PORT, '0.0.0.0');
+    app.cron.startAllJobs();
   } catch (err) {
     app.log.error(err);
     process.exit(1);
