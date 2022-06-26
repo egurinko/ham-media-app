c2583e814e6d7c238e67c7a4d4d6aa19db7fed0b
egurinko <egctoru@gmail.com>
2022-06-26 16:23:00 +0900

    truncate database on test start

--- backend/tests/configs/setupDbConnection.ts
+++ backend/tests/configs/setupDbConnection.ts
@@ -1,37 +1,20 @@
-import { PrismaPromise, Prisma } from '@prisma/client';
 import { client } from '@/services/prisma';
 import { executeTest } from '../../prisma/seeds/test';
 
-const pushTruncateTransactionIfNeeded = (): PrismaPromise<any>[] => {
-  const truncateTransactions: PrismaPromise<any>[] = [];
-
-  if (truncateTransactions.length === 0) {
-    truncateTransactions.push(client.$executeRaw`SET FOREIGN_KEY_CHECKS = 0;`);
-
-    const tablenames = Prisma.dmmf.datamodel.models.map(
-      (model) => model.dbName
-    );
-
-    for (const tablename of tablenames) {
-      if (tablename !== '_prisma_migrations' && tablename !== null) {
-        try {
-          truncateTransactions.push(
-            client.$executeRawUnsafe(`TRUNCATE ${tablename};`)
-          );
-        } catch (error) {
-          console.log({ error });
-        }
-      }
-    }
-
-    truncateTransactions.push(client.$executeRaw`SET FOREIGN_KEY_CHECKS = 1;`);
-  }
-  return truncateTransactions;
-};
-
 const setup = async () => {
-  const truncateTransactions = pushTruncateTransactionIfNeeded();
-  await client.$transaction(truncateTransactions);
+  const tableNames = (
+    (await client.$queryRawUnsafe(
+      `SELECT tablename FROM pg_catalog.pg_tables WHERE hastriggers = true;`
+    )) as { tablename: string }[]
+  ).map(({ tablename }) => tablename);
+
+  await client.$transaction(
+    tableNames.map((tableName) =>
+      client.$queryRawUnsafe(
+        `TRUNCATE TABLE "${tableName}" RESTART IDENTITY CASCADE;`
+      )
+    )
+  );
   return executeTest();
 };
 
