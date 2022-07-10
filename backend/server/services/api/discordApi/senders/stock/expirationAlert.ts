2965dade464b14b84953dd8fdaa079c53d949c3f
egurinko <egctoru@gmail.com>
2022-07-10 17:30:43 +0900

    separate message line logic to share in stock alert

--- backend/server/services/api/discordApi/senders/stock/expirationAlert.ts
+++ backend/server/services/api/discordApi/senders/stock/expirationAlert.ts
@@ -1,15 +1,12 @@
 import { postStockAlert } from '@/services/api/discordApi/notifiers/stock';
 import type { PostStockAlertVariables } from './types';
+import { getProductLines } from './expiringAlert';
 
 export const postStockExpirationAlert = (
-  variables: PostStockAlertVariables
+  stocks: PostStockAlertVariables
 ): void => {
-  const lines = variables
-    .map(
-      (v) =>
-        `\n${v.product.name}の在庫id:${v.id} =>  https://ham-media-app.net/admin/products/${v.product_id}`
-    )
-    .join();
+  const lines = getProductLines(stocks);
+
   postStockAlert(
     `===== 以下の在庫の期限が切れているよ <:shock:844862291215122432> ===== ${lines}\n`
   );
