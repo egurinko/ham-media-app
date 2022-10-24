1d866924902aff36db4b00f42bfbaaa4fdf087b7
Toru Eguchi <egctoru@gmail.com>
2022-10-24 23:21:18 +0900

    Update discordUserId.ts

--- frontend/validators/discordUserId.ts
+++ frontend/validators/discordUserId.ts
@@ -2,10 +2,10 @@ export const rules = {
   required: 'discord user idを入力してください',
   minLength: {
     value: 18,
-    message: '18文字で入力してください',
+    message: '18文字以上で入力してください',
   },
   maxLength: {
-    value: 18,
-    message: '18文字で入力してください',
+    value: 19,
+    message: '19文字以内で入力してください',
   },
 };
