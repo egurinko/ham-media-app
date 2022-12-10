093ec30c7ff88bbc8c527a7745462d139fc52c6a
egurinko <egctoru@gmail.com>
2022-12-10 18:50:35 +0900

    add function to split array by specified number

---
+++ backend/server/services/functions.ts
@@ -0,0 +1,10 @@
+export const sliceByNumber = <T>(
+  array: Array<T>,
+  number: number
+): Array<Array<T>> => {
+  const length = Math.ceil(array.length / number);
+
+  return new Array(length)
+    .fill('')
+    .map((_, i) => array.slice(i * number, (i + 1) * number));
+};
