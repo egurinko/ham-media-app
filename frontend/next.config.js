1c66840678ba321f245b9a15428f12e62bf13405
egurinko <egctoru@gmail.com>
2022-12-31 17:41:02 +0900

    enable app dir function of next.js v13

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,4 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
   webpack(config) {
     config.module.rules.push({
@@ -10,5 +11,8 @@ module.exports = {
   },
   images: {
     disableStaticImages: true,
+  },
+  experimental: {
+    appDir: true,
   },
 };
