09c0162a88da5e66f4dfda553f2057e0c0dceca1
egurinko <egctoru@gmail.com>
2023-07-22 17:42:49 +0900

    create root layout

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,9 @@
+const RootLayout =
+  () =>
+  ({ children }: { children: React.ReactNode }) => (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+
+export default RootLayout;
