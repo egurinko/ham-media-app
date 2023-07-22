7574ce2b4c1e62ec196c861cb58e7d9b6af8a026
egurinko <egctoru@gmail.com>
2023-07-22 15:38:44 +0900

    remove terraform

--- backend/terraform/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  profile = "private"
-  region  = "ap-northeast-1"
-}
--- backend/terraform/version.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  required_version = "~> 1.5.3"
-  required_providers {
-    aws = ">= 5.9.0"
-  }
-}
