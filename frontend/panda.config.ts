a1d254f9d967a1bf38af71d55ab2794e46aa3937
egurinko <egctoru@gmail.com>
2023-11-26 23:19:03 +0900

    add shadow tokens

--- frontend/panda.config.ts
+++ frontend/panda.config.ts
@@ -293,6 +293,38 @@ export default defineConfig({
             },
           },
         },
+        shadows: {
+          none: {
+            value: {
+              base: 'rgba(0, 0, 0, 0.2) 0px 0px 0px 0px, rgba(0, 0, 0, 0.14) 0px 0px 0px 0px, rgba(0, 0, 0, 0.12) 0px 0px 0px 0px',
+            },
+          },
+          xs: {
+            value: {
+              base: 'rgba(0, 0, 0, 0.2) 0px 2px 1px -1px, rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px',
+            },
+          },
+          sm: {
+            value: {
+              base: 'rgba(0, 0, 0, 0.2) 0px 3px 3px -2px, rgba(0, 0, 0, 0.14) 0px 3px 4px 0px, rgba(0, 0, 0, 0.12) 0px 1px 8px 0px',
+            },
+          },
+          md: {
+            value: {
+              base: 'rgba(0, 0, 0, 0.2) 0px 3px 5px -1px, rgba(0, 0, 0, 0.14) 0px 6px 10px 0px, rgba(0, 0, 0, 0.12) 0px 1px 18px 0px',
+            },
+          },
+          lg: {
+            value: {
+              base: 'rgba(0, 0, 0, 0.2) 0px 5px 5px -3px, rgba(0, 0, 0, 0.14) 0px 8px 10px 1px, rgba(0, 0, 0, 0.12) 0px 3px 14px 2px',
+            },
+          },
+          xl: {
+            value: {
+              base: 'rgba(0, 0, 0, 0.2) 0px 7px 8px -4px, rgba(0, 0, 0, 0.14) 0px 12px 17px 2px, rgba(0, 0, 0, 0.12) 0px 5px 22px 4px',
+            },
+          },
+        },
       },
     },
     textStyles: {
