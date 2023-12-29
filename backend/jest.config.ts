9c33fc182316a20079344e8e4e3b2676aacdb408
egurinko <egctoru@gmail.com>
2023-12-29 17:04:30 +0900

    add workspace setting

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from '@jest/types';
+import type { Config } from 'jest';
 
-export default async (): Promise<Config.InitialOptions> => {
+export default async (): Promise<Config> => {
   return {
     preset: 'ts-jest',
     globals: {
