c20668df960862ebfb59e1b2ce138529bcc54d8a
egurinko <egctoru@gmail.com>
2024-01-27 16:22:50 +0900

    add internal api client

--- frontend/app/utils/client.ts
+++ frontend/app/utils/client.ts
@@ -1,9 +1,11 @@
 import { HttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc';
 import {
   NextSSRInMemoryCache,
   NextSSRApolloClient,
 } from '@apollo/experimental-nextjs-app-support/ssr';
+import { getSessionToken } from '@/app/utils/cookies';
 
 const base = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
 
@@ -11,9 +13,20 @@ const publicLink = new HttpLink({
   uri: `${base}/public_api/graphql`,
 });
 
-const internalLink = new HttpLink({
+const internalHttpLink = new HttpLink({
   uri: `${base}/internal_api/graphql`,
 });
+const internalAuthLink = setContext((_, { headers }) => {
+  const sessionToken = getSessionToken();
+
+  return {
+    headers: {
+      ...headers,
+      authorization: sessionToken ? `Bearer ${sessionToken}` : '',
+    },
+  };
+});
+const internalLink = internalAuthLink.concat(internalHttpLink);
 
 export const { getClient: getPublicClient } = registerApolloClient(
   () =>
