0e54c1f59749603b1bc24d47393cae06c629ed84
egurinko <egctoru@gmail.com>
2025-03-22 13:00:29 +0900

    replace x icon

--- frontend/app/components/organisms/consumer/AboutOfficialSNS.tsx
+++ frontend/app/components/organisms/consumer/AboutOfficialSNS.tsx
@@ -10,13 +10,8 @@ export const AboutOfficialSNS: FC<Props> = () => (
     <div
       className={css({ display: 'flex', justifyContent: 'center', gap: '4' })}
     >
-      <a href="https://twitter.com/ham_edia">
-        <Image
-          src="/twitter_icon.png"
-          alt="Twitterロゴ"
-          width={30}
-          height={30}
-        />
+      <a href="https://x.com/ham_edia">
+        <Image src="/x_icon.png" alt="xロゴ" width={30} height={30} />
       </a>
       <a href="https://ham-media.net/haminfo/line/">
         <Image src="/line_icon.png" alt="LINEロゴ" width={30} height={30} />
